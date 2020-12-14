1a43518cc46d7325fbe247f19ecf0fc26f4c001b
synt4x93 <synt4x.93@gmail.com>
2020-12-14 07:04:19 +0000

    starlte: update prop overrides to G960FXXUCFTK1
    Change-Id: I299650c708d83637bde81c381ae12efe7bf66278

--- lineage_starlte.mk
+++ lineage_starlte.mk
@@ -35,8 +35,11 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-BUILD_FINGERPRINT := samsung/starltexx/starlte:10/QP1A.190711.020/G960FXXU7DTAA:user/release-keys
+BUILD_FINGERPRINT := samsung/starltexx/starlte:10/QP1A.190711.020/G960FXXUCFTK1:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRODUCT_NAME=starltexx \
-        PRIVATE_BUILD_DESC="starltexx-user 10 QP1A.190711.020 G960FXXU7DTAA release-keys"
+    PRODUCT_NAME=starltexx \
+    PRIVATE_BUILD_DESC="starltexx-user 10 QP1A.190711.020 G960FXXUCFTK1 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.PDA=G960FXXUCFTK1

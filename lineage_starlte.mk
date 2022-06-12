6a76d86d70fb5ea7364a0766674287a868581169
pakaoraki <pakaoraki@gmx.com>
2022-06-12 19:11:49 +0200

    Add custom OTA support

--- lineage_starlte.mk
+++ lineage_starlte.mk
@@ -38,3 +38,7 @@ PRODUCT_MODEL := SM-G960F
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# OTA
+PRODUCT_PROPERTY_OVERRIDES += \
+    lineage.updater.uri=https://raw.githubusercontent.com/pakaoraki/OTA/lineage-19.1/starlte.json
